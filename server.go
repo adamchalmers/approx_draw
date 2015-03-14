1437adfadbf814c0c75c8810273457ef5676a501
Adam <adam.s.chalmers@gmail.com>
2015-03-15 02:56:37 +1100

    Server can now read local images.

--- server.go
+++ server.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/png"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 )
 
-type Color [3]int
-type Rect struct {
-	w int32
-	h int32
-}
+var urlArg = regexp.MustCompile("url=(.*)")
 
-type String string
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the image URL from the request
+	m := urlArg.FindStringSubmatch(r.URL.String())
+	if m == nil {
+		fmt.Println("Invalid regex.")
+		return
+	}
 
-func (s String) ServeHTTP(
-	w http.ResponseWriter,
-	r *http.Request) {
-	fmt.Fprint(w, s)
+	// Open the image
+	imgfile, err := os.Open("./" + m[1])
+	if err != nil {
+		fmt.Println("Can't find", m[1])
+		return
+	}
+	defer imgfile.Close()
+
+	// Decode the image
+	img, _, err := image.Decode(imgfile)
+	if err != nil {
+		log.Fatal("Couldn't read from image.")
+	}
+
+	// Extract color data from the decoded image
+	bounds := img.Bounds()
+	rgba := ""
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			rgba += fmt.Sprintf("[%v, %v, %v, %v],", r, g, b, a)
+		}
+	}
+
+	// Return this data.
+	b := make([]byte, len(rgba))
+	copy(b, rgba)
+	w.Write(b)
 }
 
 func fileHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path, r.URL.Path[1:])
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
@@ -30,6 +59,7 @@ func main() {
 	fmt.Println("Running on", port)
 
 	http.HandleFunc("/", fileHandler)
+	http.HandleFunc("/image/", imageHandler)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal(err)
